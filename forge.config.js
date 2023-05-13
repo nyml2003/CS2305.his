58be8af0af493adcb5022891ed0e43a3c4d7b7d7
nyml2003 <2492181618@qq.com>
2023-05-14 04:09:55 +0800

    s
    Signed-off-by: nyml2003 <2492181618@qq.com>

--- forge.config.js
+++ forge.config.js
@@ -7,24 +7,6 @@ module.exports = {
       name: '@electron-forge/maker-zip',
       certificateFile: './cert.pfx',
       certificatePassword: process.env.CERTIFICATE_PASSWORD
-    },
-    {
-      // Windows
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        authors: '蒋钦禹',
-        certificateFile: './cert.pfx',
-        certificatePassword: process.env.CERTIFICATE_PASSWORD
-      }
-    },
-    {
-      name: '@electron-forge/maker-wix',
-      config: {
-        language: 2052,
-        manufacturer: '蒋钦禹',
-        certificateFile: './cert.pfx',
-        certificatePassword: process.env.CERTIFICATE_PASSWORD
-      }
     }
   ]
 };
